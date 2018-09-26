533e85d4d2b9f51c324a8eb0c6595d2babb0a93a
lkkushan101 <kam@codehousegroup.com>
2018-09-26 07:15:29 +0530

    v1

---
+++ selenium_tryouts/CheckBrokenLinks/src/main/java/SeleniumBasics/CheckBrokenLinks.java
@@ -0,0 +1,25 @@
+package SeleniumBasics;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CheckBrokenLinks {
+	
+	 public static int getResponseCode(String link) {
+	        URL url;
+	        HttpURLConnection connection = null;
+	        Integer responsecode = 0;
+	        try {
+	            url = new URL(link);
+	            connection = (HttpURLConnection) url.openConnection();
+	            responsecode = connection.getResponseCode();
+	        } catch (Exception e) {
+	           
+	        } finally {
+	            if (null != connection)
+	            	connection.disconnect();
+	        }
+	        return responsecode;
+	    }
+
+}
