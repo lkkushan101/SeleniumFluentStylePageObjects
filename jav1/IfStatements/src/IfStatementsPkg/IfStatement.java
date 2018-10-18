2831eb4c0e6e27de67ed5791696d50dde90fb22a
lkkushan101 <kam@codehousegroup.com>
2018-10-18 05:43:55 +0530

    v1

---
+++ jav1/IfStatements/src/IfStatementsPkg/IfStatement.java
@@ -0,0 +1,20 @@
+package IfStatementsPkg;
+
+public class IfStatement {
+
+	public static void main (String args[])
+	{
+		int x = 10;
+		int y = 12;
+		
+		if (x<y)
+		{
+			System.out.println("X is less than y....");
+		}
+		else
+		{
+			System.out.println("X is greater than y....");
+		}
+	}
+
+}
