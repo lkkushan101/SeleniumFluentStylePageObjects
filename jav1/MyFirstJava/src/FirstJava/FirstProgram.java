6bab0b0e95cc50cdff4f1e1584bb48df596b7006
lkkushan101 <kam@codehousegroup.com>
2018-10-18 05:37:01 +0530

    v1

---
+++ jav1/MyFirstJava/src/FirstJava/FirstProgram.java
@@ -0,0 +1,11 @@
+package FirstJava;
+
+public class FirstProgram {
+	
+	public static void main (String args[])
+	{
+		System.out.println("Hello Java....");
+		System.out.println("I'm new to Java...");
+	}
+
+}
